543df05b92b9d93b3a1aa352244f5376688e3bbc
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-07-29 00:01:32 +0100

    Added responsiveness. Send button display result on console.

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,8 +8,10 @@ let closeModal = $('.close-modal');
 let data2 = null;
 let localArray = [];
 let arr = [];
+let sendData = $('.send-data');
 
 
+//GET INFO FROM JSON AND POPULATES DROPDOWNS
 $.getJSON('dist/js/csvjson.json', function(data) {
     $.each(data, function (key, entry) {
         localDropdown.append($('<option data-key="'+key+'">'+entry.localidade+'</option>'));
@@ -17,31 +19,52 @@ $.getJSON('dist/js/csvjson.json', function(data) {
         //console.log(entry.localidade);
     });
 
+    //DUPLICATES DATA ARRAY
     data2 = [... data];
 
 });
 
+//RUNS ON CLICK 
 localList.click(function() {
-    localArray = [];
-    localArray.push($(".local-list :selected").val());
+    localArray = []; // CLEARS SELECTED ARRAY
+    localArray.push($(".local-list :selected").val()); //ADDS SELECTED OPTION TO ARRAY
 
+    //FILTERS ARRAY
     const filteredArray = data2.filter(d => d.localidade == localArray[0]);
-    console.log('Filter', filteredArray);
 
-    concessionarioDropdown.empty();
-    concessionarioDropdown.append($('<option data-key="0">Concessionário</option>'));
+    concessionarioDropdown.empty(); //CLEARS DROPDOWN
+    concessionarioDropdown.append($('<option data-key="0">Concessionário</option>')); //ADDS DEFAULT OPTION TO DROPDOWN
+    
+    //ADDS FILTERED OPTIONS TO DROPDOWN FROM FILTERED ARRAY
     $.each(filteredArray, function (key, entry) {
         concessionarioDropdown.append($('<option data-key="'+key+'">'+entry.name+'</option>'));
     });
-
-    console.log(localArray);
 });
 
 
+//OPENS MODAL
 legalBtn.click(function() {
     legalModal.addClass('legal-on');
 });
 
+//CLOSES MODAL
 closeModal.click(function() {
     legalModal.removeClass('legal-on');
+});
+
+// HELPS MODAL FROM NOT SHOWING AT START
+$( document ).ready(function() {
+    legalModal.show();
+    legalModal.css('display', 'flex');
+});
+
+//SENDS FORM DATA TO CONSOLE AS TABLE
+sendData.click(function() {
+    console.table([
+        ["Nome", $(".form-name").val()],
+        ["Telefone", $(".form-tel").val()],
+        ["Localidade", $(".local-list").val()],
+        ["Email", $(".form-mail").val()],
+        ["Concessionário", $(".concessionario-list").val()],
+    ]);
 });
